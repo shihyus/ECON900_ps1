a8e686793af8fdd3c26ac52554f5eecff31b7c5b
Shih YU <shihyus@clemson.edu>
2019-04-20 23:56:17 -0400

    try to debug

--- parse.py
+++ parse.py
@@ -22,39 +22,59 @@ def openlink(link):
 	currencies_tbody = currencies_table.find("tbody")
 	#print(currencies_tbody)
 	currencies_rows = currencies_tbody.find_all("tr")
+	dayVolume = "None"  #for errors
+	high = "None"
+	low = "None"
+	openPrice = "None"
+	closePrice = "None"
 
 	for r in currencies_rows:
 		p = r.find("th").get_text()
 		p = p.replace("\r","")
 		p = p.replace("\n","")
+		p = p.replace(" ","")
 		#print("Right =",p,type(p))
 
-		if (p == "Market Cap"):
-			q = r.find_all('span',{'data-currency-value':""})
-			marketCap = q[1].text
+		#if (p == "Market Cap"):
+			#q = r.find_all('span',{'data-currency-value':""})par
+			#marketCap = q[1].text
 			#print (p , q[1].text)
 			#openvalue = q[0]
 			#closevalue = q[1]
 			#print (openvalue, closevalue)
-		elif (p == "24 Hour Volume"):
+		if (p == "24HourVolume"):
 			q = r.find_all('span',{'data-currency-value':""})
 			#print (p, q[1].text)
 			dayVolume = q[1].text
 
-		elif (p == "Yesterday's High / Low"):
+		elif (p == "Bitcoin Price"):
+			q = r.find_all('span',{'data-currency-value':""})
+			price = q[1].text
+
+		elif (p == "Market Cap"):
+			q = r.find_all('span',{'data-currency-value':""})
+			marketCap = q[1].text
+
+		elif (p == "Yesterday'sHigh/Low"):
 			q = r.find_all('span',{'data-currency-value':""})
 			high = q[1].text
 			low = q[4].text
 			#print("High =", high, "Low =", low)
 
-		elif (p == "Yesterday's Open / Close"):
+		elif (p == "Yesterday'sOpen/Close"):
 			q = r.find_all('span',{'data-currency-value':""})
 			openPrice = q[1].text
 			closePrice = q[4].text
-			#print("Open price =", openPrice, "Close price =", closePrice)
 
-	time.sleep(10)
-	return marketCap, dayVolume, high, low, openPrice, closePrice
+
+			#print("Open price =", openPrice, "Close price =", closePrice)
+	l = [dayVolume, high, low, openPrice, closePrice]
+	print ("l = ", l)
+	time.sleep(7.2)
+	for i in l:
+		if (i == "None"):
+			print ("error result = ", l, currencies_rows)
+	return price, marketCap, dayVolume, high, low, openPrice, closePrice
 
 
 if not os.path.exists("parsed_results"):
@@ -79,13 +99,14 @@ for one_file_name in glob.glob("html_files/*.html"):
 		currency_short_name = r.find("td", {"class": "currency-name"}).find("span", {"class": "currency-symbol"}).find("a").text
 		#print(currency_short_name)
 		currency_name = r.find("td", {"class":"currency-name"}).find("a",{"class":"currency-name-container"}).text
-		currency_market_cap = r.find("td", {"class": "market-cap"})['data-sort']
-		currency_price = r.find("a", {"class": "price"}).text
+		#currency_market_cap = r.find("td", {"class": "market-cap"})['data-sort']
+		#currency_price = r.find("a", {"class": "price"}).text
 		#currency_volume = r.find("a", {"class": "volume"}).text
 		currency_link = r.find("td", {"class":"currency-name"}).find('a',href = True)['href']
 		# print(currency_link)
 		#print(currency_link)
-		marketCap, dayVolume, high, low, openPrice, closePrice = openlink(currency_link)
+		price, marketCap, dayVolume, high, low, openPrice, closePrice = openlink(currency_link)
+
 		#newcoin_table
 		#coinpage = openlink(currency_link)
 		
@@ -97,10 +118,10 @@ for one_file_name in glob.glob("html_files/*.html"):
 			'scrapping_time': scrapping_time, 
 			'short_name': currency_short_name,
 			'name': currency_name,
-			'market_cap': currency_market_cap,
-			'price': currency_price,
+			'market_cap': marketCap,
+			'price': price,
 			'volume': dayVolume,
-			'marketCap' : marketCap,
+			#'marketCap' : marketCap,
 			#'dayVolume' : dayVolume,
 			'high' : high,
 			'low' : low,
@@ -109,9 +130,9 @@ for one_file_name in glob.glob("html_files/*.html"):
 			#'supply': currency_supply,
 			#'24H_change': currency_change
 			}, ignore_index=True)
-		print ("length of df =", len(df))
-		if len(df) > 3:
-			break
+		#print ("length of df =", len(df))
+		#if len(df) > 10:
+		#	break
 
 
 
